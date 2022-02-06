0f3e9cd00dc98669c3a5f0415db9a0b002d1e169
DmitryIvanov17 <Akelka4314312@gmail.com>
2022-02-06 14:28:55 +0300

    append push / pop functions

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -8,7 +8,7 @@
 #include <malloc.h>
 
 void sizeError(size_t v) {
-    if (v == NULL) {
+    if (v == 0) {
         fprintf(stderr, "bad alloc ");
         exit(1);
     }
@@ -16,7 +16,7 @@ void sizeError(size_t v) {
 
 vector createVector(size_t n) {
     int *p = malloc(sizeof(int) * n);
-    sizeError(p);
+    sizeError((size_t)p);
     return (vector) {p, 0, n};
 }
 
@@ -28,7 +28,7 @@ void reserve(vector *v, size_t newCapacity) {
             v->size = newCapacity;
         else {
             int *vector = realloc(v->data, sizeof(int) * newCapacity);
-            sizeError(vector);
+            sizeError((size_t)vector);
         }
         v->capacity = newCapacity;
     }
@@ -44,4 +44,35 @@ void shrinkToFit(vector *v) {
 
 void deleteVector(vector *v) {
     free(v);
+}
+
+bool isEmpty(vector *v) {
+    return v->size == 0;
+}
+
+bool isFull(vector *v) {
+    return v->size == v->capacity;
+}
+
+int getVectorValue(vector *v, size_t i) {
+    return v->data[i];
+}
+
+void pushBack(vector *v, int x) {
+    if (isFull(v)) {
+        reserve(v, v->capacity * 2);
+    } else if (v->capacity == 0) {
+        reserve(v, 1);
+    }
+    v->data[v->size] = x;
+    v->size++;
+}
+
+void popBack(vector *v) {
+    if (isEmpty(v)) {
+        fprintf(stderr, "vector is empty ");
+        exit(1);
+    } else {
+        v->size--;
+    }
 }
--- main.c
+++ main.c
@@ -1,20 +1,62 @@
 #include <stdio.h>
 #include "libs/data_structures/vector/vector.h"
+#include <assert.h>
+
+void test_pushBack_emptyVector() {
+vector v = createVector(5);
+    pushBack(&v, 42);
+    assert(v.data[0] == 42);
+    assert(v.size == 1);
+    assert(v.capacity == 5);
+    deleteVector(&v);
+}
+
+void test_pushBack_fullVector() {
+    vector v = createVector(5);
+    v.size = 5;
+    pushBack(&v, 42);
+    assert(v.data[5] == 42);
+    assert(v.size == 6);
+    assert(v.capacity == 10);
+    deleteVector(&v);
+}
+
+void test_popBack_notEmptyVector() {
+    vector v = createVector(0);
+    pushBack(&v, 10);
+    assert(v.size == 1);
+    popBack(&v);
+    assert(v.size == 0);
+    assert(v.capacity == 1);
+    deleteVector(&v);
+}
+
+void test() {
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+}
 
 int main() {
-    vector v = createVector(9);
-    v.size = 3;
-    printf("%d %d \n", v.size, v.capacity);
-    reserve(&v, 2);
-    printf("%d %d %d\n", v.size, v.capacity, v.data);
-    clear(&v);
-    printf("%d %d \n", v.size, v.capacity);
-    v.size = 2;
-    v.capacity = 4;
-    shrinkToFit(&v);
-    printf("%d %d \n", v.size, v.capacity);
-    deleteVector(&v);
 
+    test();
+//    vector v = createVector(9);
+//    v.size = 3;
+//    printf("%d %d \n", v.size, v.capacity);
+//    reserve(&v, 2);
+//    printf("%d %d %d\n", v.size, v.capacity, v.data);
+//    clear(&v);
+//    printf("%d %d \n", v.size, v.capacity);
+//    v.size = 2;
+//    v.capacity = 4;
+//    shrinkToFit(&v);
+//    printf("%d %d \n", v.size, v.capacity);
+//    printf("%d\n", isEmpty(&v));
+//    printf("%d\n", isFull(&v));
+//
+//    deleteVector(&v);
+
+    printf(":)");
 
     return 0;
 }
